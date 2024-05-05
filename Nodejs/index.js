274b1d5d99f70f02d04bc99917d57d7e1ef4c8f3
saumanarsad <sauman.arshad@gmail.com>
2024-05-06 02:08:39 +0500

    Middleware & error handling

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express(); 
 
+app.use(function(req,res,next){  //Middleware
+    console.log("Middleware");
+    next();
+});
+
 app.get('/',function(req,res){
     res.send('hello world');
 });
 
-app.get('/profile',function(req,res){
+app.get('/profile',function(req,res,next){
+    return next(new Error("Something is not right"));
     res.send('my profile');
 });
-
+app.use(function(err,req,res,next){
+    console.error(err.stack);
+    res.status(500).send('Something broke');
+})
 
 app.listen(3000);
