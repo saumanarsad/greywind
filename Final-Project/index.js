6ec23383593c5adf1fc8dc1d87b6628efd1fe38c
saumanarsad <sauman.arshad@gmail.com>
2024-05-18 17:26:42 +0500

    Install tailwind

--- Final-Project/index.js
+++ Final-Project/index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,'public')));  //All static files will be found here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));  //All static files will be found here
 
 
 
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
     res.render('index');
 });
 
