f2619f0b1e0ab977b0e0564a331cb071fec27c5d
saumanarsad <sauman.arshad@gmail.com>
2024-05-18 18:19:33 +0500

    few bits

--- Final-Project/index.js
+++ Final-Project/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -10,7 +11,14 @@ app.use(express.static(path.join(__dirname, 'public')));  //All static files wil
 
 
 app.get('/', function (req, res) {
-    res.render('index');
-});
+    fs.readdir('./files', function (err, files) {
+        res.render('index', { myfiles: files });
+    })
 
+});
+app.post('/create', function (req, res) {
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function (err) {
+        res.redirect('/');
+    });
+})
 app.listen(3000);
