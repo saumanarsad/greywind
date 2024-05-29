6129b074dbda586f149ef5f9847c36e59f2bdcf6
saumanarsad <sauman.arshad@gmail.com>
2024-05-30 01:05:26 +0500

    Components

--- dagger/src/index.js
+++ dagger/src/index.js
@@ -4,9 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function MyButton() {
+  return <button>My Custom Button</button>
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <MyButton />
+    <App />
     <App />
   </React.StrictMode>
 );
