eb18fef6f7da9fcc1536bce7812f9a951273d407
saumanarsad <sauman.arshad@gmail.com>
2024-05-30 01:40:17 +0500

    Typescript Components

---
+++ dagger-ts/src/components/Todo/TodoItem/TodoItem.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import './TodoItem.css';
+
+const TodoItem: React.FC = () => {
+    return (
+        <li>I am a todo list</li>
+    );
+}
+export default TodoItem;
---
+++ dagger-ts/src/components/Todo/TodoList.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import TodoItem from "./TodoItem/TodoItem";
+import './TodoList.css'
+const TodoList: React.FC = () => {
+    return (
+        <div className="todo_container">
+            <ol>
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+            </ol>
+        </div>
+    );
+}
+
+export default TodoList
