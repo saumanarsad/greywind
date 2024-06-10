5736980af86b43b866542b6111c340c853b6fcaf
saumanarsad <sauman.arshad@gmail.com>
2024-06-11 01:23:13 +0500

    Game implemented

--- tic-tac/src/App.tsx
+++ tic-tac/src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Block from './components/Block';
+import { stat } from 'fs';
 
 function App() {
+  const [state, setState] = useState(Array(9).fill(null))
+  const [currentTurn, setCurrentTurn] = useState('X');
+
+  const HandleBlockClick = (index: number) => {
+    const StateCopy = Array.from(state);
+    StateCopy[index] = currentTurn;
+    setCurrentTurn(currentTurn === 'X' ? '0' : 'X');
+    setState(StateCopy);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='board'>
+      <div className='row'>
+        <Block onClick={() => HandleBlockClick(0)} value={state[0]} />
+        <Block onClick={() => HandleBlockClick(1)} value={state[1]} />
+        <Block onClick={() => HandleBlockClick(2)} value={state[2]} />
+      </div>
+
+      <div className='row'>
+        <Block onClick={() => HandleBlockClick(3)} value={state[3]} />
+        <Block onClick={() => HandleBlockClick(4)} value={state[4]} />
+        <Block onClick={() => HandleBlockClick(5)} value={state[5]} />
+      </div>
+      <div className='row'>
+        <Block onClick={() => HandleBlockClick(6)} value={state[6]} />
+        <Block onClick={() => HandleBlockClick(7)} value={state[7]} />
+        <Block onClick={() => HandleBlockClick(8)} value={state[8]} />
+      </div>
+
     </div>
   );
 }
---
+++ tic-tac/src/components/Block.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './block.css';
+
+interface BlockProps {
+    value?: string | null;
+    onClick?: () => void
+}
+
+const Block: React.FC<BlockProps> = (props) => {
+    return (
+        <div onClick={props.onClick} className="block ">{props.value}</div>
+    );
+}
+
+export default Block;
