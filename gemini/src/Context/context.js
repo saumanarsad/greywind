55f5b4a8646539c532e0c5aa039f10f419cd5bb8
saumanarsad <sauman.arshad@gmail.com>
2024-06-21 03:32:33 +0500

    New files

---
+++ gemini/src/Context/context.js
@@ -0,0 +1,14 @@
+import { createContext } from 'react';
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+    const contextValue = {
+
+    }
+    return (
+        <Context.Provider>
+
+        </Context.Provider>
+    )
+}
